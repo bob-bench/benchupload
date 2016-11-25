b7400b36cd585964a9d24bf2ae37b223f17af5b0
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2016-11-25 20:33:50 +0100

    bobbench: Initial packaging of the python script
    Minimal setup.py to be able to install the cli utility.

---
+++ setup.py
@@ -0,0 +1,23 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Collect xUnit xml files and upload them to bob-bench.org',
+    'author': 'Holger Hans Peter Freyther',
+    'url': 'http://www.bob-bench.org',
+    'download_url': 'http://www.bob-bench.org',
+    'author_email': 'help@bob-bench.org',
+    'version': '2',
+    'install_requires': [
+        'requests',
+    ],
+    'license': 'AGPLv3+',
+    'packages': ['benchupload'],
+    'scripts': [],
+    'entry_points': {'console_scripts': ['benchupload=benchupload.__main__:main']},
+    'name': 'benchupload'
+}
+
+setup(**config)
