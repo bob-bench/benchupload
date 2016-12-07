a6f82aa680079e21f45ac6cca1dcd07e17869c57
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2016-12-07 23:38:55 +0100

    test: First test to detect travis-ci
    Add simple shell script for manual invocation of the benchupload,
    add separate requirements file, add testcase to manipulate the env
    and detect travis-ci

---
+++ tests/tests.py
@@ -0,0 +1,43 @@
+import os
+import copy
+import unittest
+from benchupload import cidetect
+
+class TestCIDetect(unittest.TestCase):
+    def setUp(self):
+        self.env = copy.deepcopy(os.environ)
+        self.cleanEnv()
+
+    def tearDown(self):
+        os.environ = self.env
+
+    def cleanEnv(self):
+        """Remove all non supported environments"""
+        valid_keys = ['_', 'TERM', 'Apple_PubSub_Socket_Render', 'SHLVL', 'SSH_AUTH_SOCK', 'SECURITYSESSIONID', 'TERM_SESSION_ID', 'TERM_PROGRAM_VERSION', 'LC_CTYPE', '__CF_USER_TEXT_ENCODING', 'PWD', 'SHELL', 'LOGNAME', 'USER', 'XPC_SERVICE_NAME', 'HOME', 'PATH', 'XPC_FLAGS', 'DISPLAY', 'TMPDIR', 'TERM_PROGRAM']
+        for key in copy.deepcopy(os.environ.keys()):
+            if not key in valid_keys:
+                del os.environ[key]
+
+    def testDetectTravisCI(self):
+        os.environ['CI'] = 'true'
+        os.environ['TRAVIS'] = 'true'
+        os.environ['TRAVIS_REPO_SLUG'] = 'bobbench/citool'
+        os.environ['TRAVIS_COMMIT'] = '1234abcdef'
+        os.environ['TRAVIS_COMMIT_RANGE'] = '???'
+        os.environ['TRAVIS_TAG'] = ''
+        os.environ['TRAVIS_BRANCH'] = 'branchname'
+        os.environ['TRAVIS_JOB_NUMBER'] = '4.1'
+        os.environ['TRAVIS_OS_NAME'] = 'linux'
+
+        res = cidetect.detect()
+        self.assertIsNotNone(res)
+        self.assertEquals(res.system_name(), "travis-ci")
+        self.assertTrue(res.is_github)
+        self.assertEquals(res.commit, '1234abcdef')
+        self.assertEquals(res.commit_range, '???')
+        self.assertEquals(res.repo_url, 'https://github.com/bobbench/citool')
+        self.assertEquals(res.tag, '')
+        self.assertEquals(res.branch, 'branchname')
+        self.assertEquals(res.build_nr, '4.1')
+        self.assertEquals(res.os_name, 'linux')
+        pass
